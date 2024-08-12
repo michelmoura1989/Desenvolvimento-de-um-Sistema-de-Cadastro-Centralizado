5bc8cddef4894d9783df5a9c29d5039d3afe07f8
michelmoura1989 <124470627+michelmoura1989@users.noreply.github.com>
2024-08-12 08:55:06 -0300

    versao2

--- trabalho.py
+++ trabalho.py
@@ -1,8 +1,15 @@
 import tkinter as tk
 
+def adicionar_cliente():
+    print("Cliente adicionado!")
+
 def main():
     root = tk.Tk()
     root.title("Sistema de Cadastro")
+    
+    btn_adicionar = tk.Button(root, text="Adicionar Cliente", command=adicionar_cliente)
+    btn_adicionar.pack()
+
     root.mainloop()
 
 if __name__ == "__main__":
